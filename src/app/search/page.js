74449d50b0eefbbfd26410ca2e1800b93eae6944
Lasha-Dagelishvili <dagelishvililasha@gmail.com>
2024-07-01 21:33:33 +0400

    created slidebar as a component

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import Navbar from "../components/navbar";
+import SlideBar from "../components/slideBar";
 
 export default function Search() {
   const searchParams = useSearchParams();
@@ -51,49 +52,7 @@ export default function Search() {
     <>
       <Navbar handleCategoryClick={handleCategoryClick} />
       <div className="flex">
-        <div className="box-border h-auto w-64 p-4 border-4">
-          <h2 className="text-xl font-bold mb-4 border-b-2 border-gray-300 pb-2">
-            Shoes for Men
-          </h2>
-          <ul className="space-y-2">
-            <li
-              className="font-semibold cursor-pointer"
-              onClick={() => handleCategoryClick("Sport")}
-            >
-              Sport
-            </li>
-            <li
-              className="font-semibold cursor-pointer"
-              onClick={() => handleCategoryClick("Business")}
-            >
-              Business
-            </li>
-            <li
-              className="font-semibold cursor-pointer"
-              onClick={() => handleCategoryClick("Casual")}
-            >
-              Casual
-            </li>
-            <li
-              className="font-semibold cursor-pointer"
-              onClick={() => handleCategoryClick("Winter")}
-            >
-              Winter
-            </li>
-            <li
-              className="font-semibold cursor-pointer"
-              onClick={() => handleCategoryClick("Flip-Flops")}
-            >
-              Flip-Flops
-            </li>
-          </ul>
-          <h2 className="text-xl font-bold mt-8 mb-4">Filter By</h2>
-          <ul className="space-y-2 border-t-2 border-gray-300 pt-4">
-            <li className="font-semibold">Size</li>
-            <li className="font-semibold">Color</li>
-            <li className="font-semibold">Price</li>
-          </ul>
-        </div>
+      <SlideBar handleCategoryClick={handleCategoryClick} />
         <div className="w-3/4 p-4">
           <h1 className="text-2xl font-bold mb-4">
             Search Results for "{query || category}"
