9efdf347a037b43e813fea659991d782a2367866
Daviti0410 <lazariashvilidato@gmail.com>
2024-07-03 18:24:47 +0400

    styled upload fields

--- src/app/components/heroSection.js
+++ src/app/components/heroSection.js
@@ -1,11 +1,11 @@
 export default function HeroSection() {
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="relative flex-1 h-96 md:h-lvh">
+      <div className="relative flex-1 h-screen md:h-lvh">
         <img
           src="/images/jeans.jpg"
           alt="Jeans"
-          className="w-full h-full object-cover"
+          className="w-full h-screen object-cover"
         />
         <div className=" absolute inset-0 flex items-center justify-center space-x-4 p-8 text-white">
           <h2 className="text-4xl font-bold text-shadow cursor-default">
--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState } from "react";
 import Navbar from "../components/navbar";
 
@@ -13,8 +12,8 @@ export default function Upload() {
   const [condition, setCondition] = useState("");
   const [color, setColor] = useState("");
   const [stock, setStock] = useState("");
+  const [price, setPrice] = useState("");
   const [message, setMessage] = useState("");
-  const [price, setPrice] = useState(0);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -55,147 +54,123 @@ export default function Upload() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <div className="w-full max-w-lg p-8 space-y-3 rounded-lg bg-white shadow-lg">
-          <h1 className="text-xl font-bold">Upload Clothes</h1>
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div className="flex flex-col space-y-3">
-              <label className="text-sm font-medium text-gray-700">Title</label>
+      <div className="flex flex-col items-center justify-center w-full h-auto bg-gray-100">
+        <div className="w-full max-w-7xl min-h-auto p-8 space-y-6 space-x-20 rounded-lg bg-white shadow-lg">
+          <h1 className="text-2xl font-bold text-center">Create Listing</h1>
+          <form
+            className="flex flex-wrap justify-between space-x-6"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex-1 min-w-[48%] space-y-10">
+              <select
+                type="text"
+                placeholder="Brand"
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+                required
+              >
+                <option value="">Select Brand</option>
+                <option value="nike">Nike</option>
+                <option value="adidas">Adidas</option>
+                <option value="gucci">Gucci</option>
+              </select>
+
               <input
                 type="text"
-                className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="Color"
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+                required
+              />
+              <select
+                type="text"
+                placeholder="Type"
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+                required
+              >
+                <option value="">Select Type</option>
+                <option value="formal">Formal</option>
+                <option value="casual">Casual</option>
+              </select>
+              <input
+                type="text"
+                placeholder="Name"
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
                 required
               />
-            </div>
-            <div className="flex flex-col space-y-3">
-              <label className="text-sm font-medium text-gray-700">
-                Description
-              </label>
               <textarea
-                className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="Description"
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
                 required
               ></textarea>
             </div>
-            <div className="flex flex-col space-y-3">
-              <label className="text-sm font-medium text-gray-700">Image</label>
+            <div className="flex-1 min-w-[48%] space-y-10">
+              <div className="w-full h-32 border-2 border-dashed border-gray-300 flex items-center justify-center">
+                <label className="cursor-pointer">
+                  <span className="text-gray-500">Click to upload</span>
+                  <input
+                    type="file"
+                    className="hidden"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    required
+                  />
+                </label>
+              </div>
+              <select
+                type="text"
+                placeholder="Category"
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                required
+              >
+                <option value="">Select Category</option>
+                <option value="clothing">Clothing</option>
+                <option value="accessories">Accessories</option>
+                <option value="footwear">Footwear</option>
+              </select>
+              <select
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={condition}
+                onChange={(e) => setCondition(e.target.value)}
+                required
+              >
+                <option value="">Select Condition</option>
+                <option value="new">New</option>
+                <option value="used">Used</option>
+              </select>
               <input
-                type="file"
-                className="text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
-                onChange={(e) => setFile(e.target.files[0])}
+                type="number"
+                placeholder="Price"
+                className="input-number w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={price}
+                onChange={(e) => {
+                  const newValue = e.target.value;
+                  setPrice(newValue < 0 ? 0 : newValue);
+                }}
+                min={0}
+                required
+              />
+              <input
+                type="number"
+                placeholder="No of Stock"
+                className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
                 required
               />
             </div>
-            <div className="flex flex-wrap gap-4 justify-between">
-              <div className="flex-1 min-w-[240px] flex flex-col space-y-3">
-                <label className="text-sm font-medium text-gray-700">
-                  Category
-                </label>
-                <select
-                  className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                  required
-                >
-                  <option value="">Select Category</option>
-                  <option value="clothing">Clothing</option>
-                  <option value="accessories">Accessories</option>
-                  <option value="footwear">Footwear</option>
-                </select>
-              </div>
-              <div className="flex-1 min-w-[240px] flex flex-col space-y-3">
-                <label className="text-sm font-medium text-gray-700">
-                  Brand
-                </label>
-                <select
-                  className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  value={brand}
-                  onChange={(e) => setBrand(e.target.value)}
-                  required
-                >
-                  <option value="">Select Brand</option>
-                  <option value="nike">Nike</option>
-                  <option value="adidas">Adidas</option>
-                  <option value="gucci">Gucci</option>
-                </select>
-              </div>
-            </div>
-            <div className="flex flex-wrap gap-4 justify-between">
-              <div className="flex-1 min-w-[240px] flex flex-col space-y-3">
-                <label className="text-sm font-medium text-gray-700">
-                  Type
-                </label>
-                <select
-                  className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  value={type}
-                  onChange={(e) => setType(e.target.value)}
-                  required
-                >
-                  <option value="">Select Type</option>
-                  <option value="formal">Formal</option>
-                  <option value="casual">Casual</option>
-                </select>
-              </div>
-              <div className="flex-1 min-w-[240px] flex flex-col space-y-3">
-                <label className="text-sm font-medium text-gray-700">
-                  Condition
-                </label>
-                <select
-                  className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  value={condition}
-                  onChange={(e) => setCondition(e.target.value)}
-                  required
-                >
-                  <option value="">Select Condition</option>
-                  <option value="new">New</option>
-                  <option value="used">Used</option>
-                </select>
-              </div>
-            </div>
-            <div className="flex flex-wrap gap-4 justify-between">
-              <div className="flex-1 min-w-[240px] flex flex-col space-y-3">
-                <label className="text-sm font-medium text-gray-700">
-                  Colour
-                </label>
-                <input
-                  type="text"
-                  className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  value={color}
-                  onChange={(e) => setColor(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="flex-1 min-w-[240px] flex flex-col space-y-3">
-                <label className="text-sm font-medium text-gray-700">
-                  Price
-                </label>
-                <input
-                  type="number"
-                  className="input-number px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  value={price}
-                  onChange={(e) => setPrice(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="flex-1 min-w-[240px] flex flex-col space-y-3">
-                <label className="text-sm font-medium text-gray-700">
-                  No of Stock
-                </label>
-                <input
-                  type="number"
-                  className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                  value={stock}
-                  onChange={(e) => setStock(e.target.value)}
-                  required
-                />
-              </div>
-            </div>
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="mt-4 w-32 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Upload
             </button>
