936e7286e8f905bbb8ff7c006a847441388185b6
Daviti0410 <lazariashvilidato@gmail.com>
2024-07-09 18:02:11 +0400

    implemented year functional

--- src/app/signUp/page.js
+++ src/app/signUp/page.js
@@ -1,5 +1,6 @@
 export default function signUp() {
   const days = Array.from({ length: 31 }, (_, i) => ++i);
+  const years = Array.from({ length: 2024 - 1970 + 1 }, (_, i) => 1970 + i);
 
   return (
     <>
@@ -73,11 +74,13 @@ export default function signUp() {
                 <option>November</option>
                 <option>December</option>
               </select>
-              <select
-                class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                id="birth-year"
-              >
-                <option>Year</option>
+              <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                <option value="">Year</option>
+                {years.map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
