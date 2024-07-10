3a60e37c5bf50e2641a2d6a3907af632a1eb1e53
Lasha-Dagelishvili <dagelishvililasha@gmail.com>
2024-07-10 21:45:08 +0400

    initial commit

--- src/app/signUp/page.js
+++ src/app/signUp/page.js
@@ -1,52 +1,53 @@
+
 export default function signUp() {
   const days = Array.from({ length: 31 }, (_, i) => ++i);
   const years = Array.from({ length: 2024 - 1970 + 1 }, (_, i) => 1970 + i);
 
   return (
     <>
-      <div class="max-w-xl mx-auto bg-gray-100 p-8 rounded-lg shadow-md">
+      <div className="max-w-xl mx-auto bg-gray-100 p-8 rounded-lg shadow-md">
         <form>
-          <div class=" flex justify-center mb-5 text-gray-700 font-bold text-4xl">
+          <div className=" flex justify-center mb-5 text-gray-700 font-bold text-4xl">
             Registration
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="first-name"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlfor="first-name"
             >
               First Name
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="first-name"
               type="text"
               placeholder="First Name"
             />
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="last-name"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlfor="last-name"
             >
               Last Name
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="last-name"
               type="text"
               placeholder="Last Name"
             />
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="birth-date"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlfor="birth-date"
             >
               Birth Date
             </label>
-            <div class="flex space-x-2">
+            <div className="flex space-x-2">
               <select
-                class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 id="birth-day"
               >
                 <option>day</option>
@@ -57,7 +58,7 @@ export default function signUp() {
                 ))}
               </select>
               <select
-                class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 id="birth-month"
               >
                 <option>Month</option>
@@ -74,7 +75,7 @@ export default function signUp() {
                 <option>November</option>
                 <option>December</option>
               </select>
-              <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+              <select classNameName="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                 <option value="">Year</option>
                 {years.map((year) => (
                   <option key={year} value={year}>
@@ -84,78 +85,75 @@ export default function signUp() {
               </select>
             </div>
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="phone-number"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlfor="phone-number"
             >
               Phone Number
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="phone-number"
               type="text"
               placeholder="(000) 000-0000"
             />
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlfor="email"
             >
               E-mail
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="email"
               type="email"
               placeholder="example@example.com"
             />
           </div>
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="address"
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlfor="address"
             >
               Address
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
               id="address-1"
               type="text"
               placeholder="Street Address"
             />
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
               id="city"
               type="text"
               placeholder="City"
             />
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
               id="state"
               type="text"
               placeholder="State / Province"
             />
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2"
               id="postal-code"
               type="text"
               placeholder="Postal / Zip Code"
             />
             <select
-              class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              id="country"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              id="countries" name="countries"
             >
               <option>Country</option>
-              <option>Georgia</option>
-              <option>USA</option>
-              <option>Japan</option>
             </select>
           </div>
-          <div class="flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
             >
               Submit
