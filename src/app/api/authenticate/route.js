a9330ba65b11dd0745a8ec33ba50033d23a3f3ac
Lasha-Dagelishvili <dagelishvililasha@gmail.com>
2024-07-16 22:53:09 +0400

    initial commit

---
+++ src/app/api/authenticate/route.js
@@ -0,0 +1,52 @@
+import pool from "@/app/lib/connection";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+
+
+export async function POST(req) {
+  try{
+    const data = await req.json();
+    const { email, password, } = data;
+
+    if (!email || !password){
+        return NextResponse.json(
+            { success: false, message: "Missing Required Fields" },
+            { status: 400 }
+        );
+    }
+
+    
+
+    const query = `SELECT * FROM users WHERE email = ?`;
+
+    const [rows] = await pool.execute(query, [email]);
+
+    if (rows.length === 0) {
+        return NextResponse.json(
+          { error: "Invalid email or Password" },
+          { status: 401 }
+        );
+      }
+      const user = rows[0];
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return NextResponse.json(
+          { error: "Invalid login or Password" },
+          { status: 401 }
+        );
+      }
+
+    return NextResponse.json(
+        { success: true, id: result.insertId },
+        { status: 200 }
+    );
+  } catch (error){
+        console.error(error);
+        return NextResponse.json(
+            { success: false, message: error.message },
+            { status: 500 }
+        );
+  }
+}
--- src/app/signIn/page.js
+++ src/app/signIn/page.js
@@ -1,52 +1,68 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { loginSchema } from "../lib/validation";
+import { useRouter } from "next/navigation";
 
-export default function signIn() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const signIn = () => {
+  const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const router = useRouter();
 
-    const formData = new FormData();
-    formData.append("username", username);
-    formData.append("password", password);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: async (values) =>{
+      try{
+        const res = await fetch("/api/authenticate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
 
-    try {
-      const res = await fetch("/api/createUser", {
-        method: "POST",
-        body: formData,
-      });
-
-      const data = await res.json();
-      if (data.success) {
-        setMessage("Upload successful!");
-      } else {
-        setMessage("Upload faild:" + data.message);
+        const data = await res.json();
+        if (data.success) {
+          setMessage("Log IN Successfull");
+          router.push("/");
+        } else {
+          setMessage("Log In Failed: " + data.message);
+        }
+      } catch (error) {
+        setMessage("Log In Failed: " + error.message);
       }
-    } catch (error) {
-      setMessage("Upload failed: " + error.message);
-    }
-  };
+    },
+  });
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md">
         <h2 className="flex justify-center text-2xl font-bold mb-2">Sign in</h2>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               Email or Phone
             </label>
             <input
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
               placeholder="Email or Phone"
             />
+            {formik.touched.email && formik.errors.email ? (
+            <div className="text-red-500 text-xs">
+              {formik.errors.email}
+            </div>
+          ) : null}
           </div>
 
           <div className="mb-4">
@@ -55,12 +71,19 @@ export default function signIn() {
             </label>
             <div className="relative">
               <input
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
+                type="password"
                 placeholder="Password"
               />
+              {formik.touched.password && formik.errors.password ? (
+            <div className="text-red-500 text-xs">
+              {formik.errors.password}
+            </div>
+          ) : null}
               <span className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 cursor-pointer"></span>
             </div>
           </div>
@@ -76,10 +99,10 @@ export default function signIn() {
               className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              Sign in
+              Sign In
             </button>
           </div>
-
+          </form>
           <div className="flex items-center justify-center mb-4">
             <hr className="flex-grow border-gray-300" />
             <span className="px-2 text-gray-500">or</span>
@@ -89,13 +112,17 @@ export default function signIn() {
           <div className="mb-4">
             <button
               className="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
+              type="button"
+              onClick={() => router.push("/signUp")}
             >
+
               Create New Account
             </button>
           </div>
-        </form>
+        
       </div>
     </div>
   );
 }
+
+export default signIn;
