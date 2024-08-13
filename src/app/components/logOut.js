4386c842524af9e056b8e789eaeffbcf1d6acf43
Daviti0410 <lazariashvilidato@gmail.com>
2024-08-13 13:01:29 +0400

    initial commit

--- src/app/components/logOut.js
+++ src/app/components/logOut.js
@@ -1,14 +1,9 @@
-import { useRouter } from "next/navigation";
-
 function logOut() {
-  const router = useRouter();
-
   const handleLogOut = async () => {
     await fetch("/api/logOut", {
       method: "POST",
     });
     window.location.reload();
-    router.push("/");
   };
 
   return <button onClick={handleLogOut}>Logout </button>;
