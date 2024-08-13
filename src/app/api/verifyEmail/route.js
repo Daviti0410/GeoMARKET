ad36824694ef952b39bec5512c736ab5b8fd0935
Daviti0410 <lazariashvilidato@gmail.com>
2024-08-13 16:56:21 +0400

    Add and update routes for user creation and email verification

---
+++ src/app/api/verifyEmail/route.js
@@ -0,0 +1,31 @@
+import pool from "@/app/lib/connection";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const token = searchParams.get("token");
+
+  try {
+    const query = "SELECT * FROM users WHERE verificationToken = ?";
+    const [user] = await pool.query(query, [token]);
+
+    if (user.length === 0) {
+      return NextResponse.json(
+        { success: false, message: "Invalid or expired token" },
+        { status: 400 }
+      );
+    }
+
+    const updateQuery =
+      "UPDATE users SET isVerified = 1, verificationToken = NULL WHERE id = ?";
+    await pool.query(updateQuery, [user[0].id]);
+
+    return NextResponse.redirect("/signIn");
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, message: error.message },
+      { status: 500 }
+    );
+  }
+}
